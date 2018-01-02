e760cb1bd6d2645ddc00fe849afadc6d349a4392
Manikandan <mvn271192@gmail.com>
2018-01-02 22:00:20 +0530

    add pod file for fire base

---
+++ Podfile
@@ -0,0 +1,7 @@
+# platform :ios, '9.0'
+use_frameworks!
+
+pod 'Firebase/Core'
+
+target 'mShare' do
+end
---
+++ Pods/Target Support Files/Pods-mShare/Pods-mShare-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mShare : NSObject
+@end
+@implementation PodsDummy_Pods_mShare
+@end
