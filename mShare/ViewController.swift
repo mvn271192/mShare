83f232756b36764e9d02bc1205148e6c6b7f9e13
Manikandan <mvn271192@gmail.com>
2018-01-02 21:11:07 +0530

    Initial Commit

---
+++ mShare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mShare
+//
+//  Created by Manikandan V Nair on 02/01/18.
+//  Copyright © 2018 MVN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
