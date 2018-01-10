a8a419ded07d092a14a125c682f8abd9f90ccd8a
Manikandan V. Nair <manikandan.v@esystems-imac-2.esoft.com>
2018-01-10 11:13:52 +0530

    user search view show

--- mShare/DashBoard/AddUserViewController.swift
+++ mShare/DashBoard/AddUserViewController.swift
@@ -11,7 +11,10 @@ import Firebase
 import FirebaseDatabase
 
 class AddUserViewController: UIViewController {
-
+    
+    let NameTag = 1, EmailTag = 2, PhotoTag = 3
+    
+    
     @IBOutlet weak var userSearchView: UIView!
     @IBOutlet weak var emailTextField: UITextField!
     private lazy var databaseRef: DatabaseReference = Database.database().reference()
@@ -20,7 +23,7 @@ class AddUserViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.userSearchView.isHidden = true
         // Do any additional setup after loading the view.
     }
@@ -29,42 +32,76 @@ class AddUserViewController: UIViewController {
         
     }
     
-     // MARK: - Action
+    // MARK: - Action
     
-
+    
     @IBAction func searchButtonClick(_ sender: Any)
     {
-       if  let emailText = emailTextField.text , emailText.count > 0
-       {
-        
-        databaseRef.child(USERS).queryOrdered(byChild: "email").queryEqual(toValue: emailText).observe(.value, with: { (snapshot) in
+        if  let emailText = emailTextField.text , emailText.count > 0
+        {
             
-            for item in snapshot.children
-            {
+            databaseRef.child(USERS).queryOrdered(byChild: "email").queryEqual(toValue: emailText).observe(.value, with: { (snapshot) in
                 
-               // if
-                let snap = item as! DataSnapshot
-                let dic = snap.value as! Dictionary<String,AnyObject>
-                if let name = dic["name"] as! String!
-               {
-                    let name = dic["name"] as! String!
-                    let email = dic["email"] as! String!
-                    let photoURL = dic["photoURL"] as? String ?? ""
+                for item in snapshot.children
+                {
+                    
+                   
+                    let snap = item as! DataSnapshot
+                    let dic = snap.value as! Dictionary<String,AnyObject>
+                    if let name = dic["name"] as! String!
+                    {
+                       
+                        let email = dic["email"] as! String!
+                        let photoURL = dic["photoURL"] as? String ?? ""
+                        let user = mUser(name: name, email: email!, photoURL: photoURL, uid: snap.key)
+                        self.setUserView(forUser: user)
+                    }
+                    
                 }
                 
-            }
-            
-            //let user = mUser(name: name!, email: email!, photoURL: photoURL, uid: userID!)
-        })
+            })
         }
         
     }
+    
     
     @IBAction func addUserButtonClick(_ sender: Any)
     {
         self.navigationController?.popViewController(animated: true)
     }
     
+    
+    // MARK: - Fuctions
+    
+    func setUserView(forUser user:mUser)
+    {
+        let nameLabel = userSearchView.viewWithTag(NameTag) as! UILabel
+        let emailLabel = userSearchView.viewWithTag(EmailTag) as! UILabel
+        let photoImageView = userSearchView.viewWithTag(PhotoTag) as! UIImageView
+        
+        nameLabel.text = user.name
+        emailLabel.text = user.email
+        
+        userSearchView.isHidden = false
+        
+        let photoURL = user.photoURL
+        if let url = photoURL {
+            let data = try? Data(contentsOf: url)
+            
+            DispatchQueue.main.async {
+               // activityIndicator.stopAnimating()
+                if let imgData = data
+                {
+                    let image = UIImage(data: imgData)
+                    photoImageView.image = image
+                    photoImageView.layer.cornerRadius = photoImageView.frame.size.height/2
+                    photoImageView.layer.masksToBounds = true
+                }
+            }
+        }
+        
+        
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -73,15 +110,15 @@ class AddUserViewController: UIViewController {
     
     
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
